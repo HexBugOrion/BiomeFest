6bc9c9021c3aab7a4ed69ed9365637afd9715adb
{VoxelDragon6} <58091259+HexBugOrion@users.noreply.github.com>
2023-06-04 13:23:51 -0500

    Big things are coming...

--- src/main/java/net/timeworndevs/biomefest/Main.java
+++ src/main/java/net/timeworndevs/biomefest/Main.java
@@ -1,6 +1,7 @@
 package net.timeworndevs.biomefest;
 
 import net.fabricmc.api.ModInitializer;
+import net.timeworndevs.biomefest.registry.BiomeRegistry;
 import net.timeworndevs.biomefest.registry.BlockRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +15,16 @@ public class Main implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Loading Biome Fest...");
+
+		LOGGER.info("ItemRegistry Starting");
+		LOGGER.info("Items Registered");
+
+		LOGGER.info("BlockRegistry Starting");
 		BlockRegistry.blockRegistry();
+		LOGGER.info("Blocks Registered");
+
+		LOGGER.info("BiomeRegistry Staring");
+		BiomeRegistry.biomeRegistry();
+		LOGGER.info("Biomes Registered");
 	}
 }
---
+++ src/main/java/net/timeworndevs/biomefest/registry/BiomeRegistry.java
@@ -0,0 +1,9 @@
+package net.timeworndevs.biomefest.registry;
+
+public class BiomeRegistry {
+
+    public static void biomeRegistry(){
+
+    }
+
+}
